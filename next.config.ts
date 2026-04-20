cf095b0adf2b6f14830ffb3c8d67274f063bbba6
Ahlam <Abdulalwasu@hotmail.com>
2026-04-20 10:09:23 +0200

    fix: remove invalid allowedDevOrigins experimental flag to fix build error

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
-  experimental: {
-    allowedDevOrigins: ['localhost', '127.0.0.1']
-  }
+  /* config options here */
 };
 
 export default withNextIntl(nextConfig);
